c5f239cbe33bae2996ca75b27e622865f3acd324
azaporojan <moldaviandron@gmail.com>
2023-12-02 14:07:33 +0100

    define initial page structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { renderForm } from "./renderForm.js";
+import { init } from "./init.js";
 
-document.body.appendChild(renderForm());
+init();
